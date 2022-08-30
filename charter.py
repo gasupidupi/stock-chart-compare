e418a186e4df3eda8eae5b5c7b1910ab94cc6e7f
gasupidupi <43344400+gasupidupi@users.noreply.github.com>
2022-08-30 13:14:25 +0200

    cycle colors

--- charter.py
+++ charter.py
@@ -1,5 +1,6 @@
 """Generates the chart"""
 import matplotlib.pyplot as plt
+from itertools import cycle
 
 
 class Charter():
@@ -9,11 +10,11 @@ class Charter():
         """Generates the chart"""
         plt.style.use("dark_background")
         plt.title("charts compared")
-        colors = ["red","orange","yellow","green","blue","purple"]
+        colors = cycle(["red","orange","yellow","green","blue","purple"])
         for stock in stocks:
             dtf = stock.dataframe
             normalized_dtf = (dtf-dtf.min())/(dtf.max()-dtf.min())
-            plt.plot(normalized_dtf, color=colors.pop(), label=stock.symbol)
+            plt.plot(normalized_dtf, color=next(colors), label=stock.symbol)
         plt.legend(loc="upper right")
         plt.xlabel("time (days)")
         plt.ylabel("price (normalized)")
